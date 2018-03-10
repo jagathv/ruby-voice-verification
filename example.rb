d8eccb92b8fb86a79a756110a6a8d3b25d1c4863
jagathv <jagathvytheeswaran@Jagaths-MacBook-Pro.local>
2018-03-09 16:50:58 -0800

    fixed example code

--- example.rb
+++ example.rb
@@ -7,6 +7,6 @@ url2_sample = "https://jagathv.github.io/audio_files/houston_sample_2.wav"
 url3_sample = "https://jagathv.github.io/audio_files/houston_sample_3.wav"
 url4_sample = "https://jagathv.github.io/audio_files/houston_sample_4.wav"
 
-rec.pid = create_user()
+pid = rec.create_user()
 rec.train(pid, url1_sample, url2_sample, url3_sample)
 puts rec.validate(pid, url4_sample)
